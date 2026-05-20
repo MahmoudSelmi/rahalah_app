95b54efa6852aa7adb7f8307ca2601e2d0e67f5d
Mahmoud Selmi <your_github_email@example.com>
2026-05-20 13:54:24 +0300

    new2

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,10 @@ import 'package:flutter/material.dart'
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:rahalah_app/core/driver_side/profile_screen.dart';
-import 'package:rahalah_app/welcome_screen.dart';
 
 import 'firebase_options.dart';
 
+// 2
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
